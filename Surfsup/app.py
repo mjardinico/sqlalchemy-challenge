8efe7b21ea9bba1efbdbf4701dd4d5aa802401ae
Michael Jardinico <michael.jardinico@csun.edu>
2023-11-26 21:25:06 -0800

    update app.py to include the Start and End Dates search. Added a form on the home page for user's convenience

--- Surfsup/app.py
+++ Surfsup/app.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from datetime import datetime, timedelta
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, redirect, url_for
 
 # create engine to hawaii.sqlite
 engine = create_engine("sqlite:///../Resources/hawaii.sqlite", echo=False)
@@ -35,10 +35,20 @@ def home():
     html = """
     <h1> Welcome to My Climate App </h1>
     <a href="/api/v1.0/precipitation">Precipitation Analysis</a>
-    <br>
+    <br><br>
     <a href="/api/v1.0/stations">Station Analysis</a>
-    <br>
-    <a href="/api/v1.0/tobs">Temperature Observation of Most Active Station</a>
+    <br><br>
+    <a href="/api/v1.0/tobs">TOBS of the Most Active Station</a>
+    <br><br>
+    
+    <h2>Temperature Statistics</h2>
+    <form action="/api/v1.0/temperature_stats" method="get">
+        <label for="start">Start Date:</label>
+        <input type="date" id="start" name="start">
+        <label for="end">End Date (optional):</label>
+        <input type="date" id="end" name="end">
+        <input type="submit" value="Get Temperature Stats">
+    </form>
     <br>
     """
     return html
@@ -47,7 +57,6 @@ def home():
 def precipitation():
     # Create a session link from Python to the DB
     session = Session(engine)
-    print('This is the Precipitation Page')
     
     # Query all date and precipitation scores, starting from the most recent data point
     most_recent_date = session.query(func.max(Measurement.date))
@@ -73,20 +82,127 @@ def precipitation():
         prcp_dict['prcp'] = prcp
         precipitation_data.append(prcp_dict)
         
-    # Jsonify the dictionary
+    # Return the JSON representation of your dictionary
     return jsonify(precipitation_data)
 
 @app.route('/api/v1.0/stations')
 def stations():
-    print('This is the Station Page')
-    return f'Station Page'   
+    
+    # Create a session link from Python to the DB
+    session = Session(engine)
+            
+    # # Design a query to calculate the total number of stations in the dataset
+    # station_count = session.query(Station).count()
+    
+    # Design a query to find the most active stations (i.e. which stations have the most rows?)
+    # List the stations and their counts in descending order.
+    station_query = session.query(Measurement.station,func.count(Measurement.station).label('count'))\
+                    .group_by(Measurement.station).order_by(func.count(Measurement.station).desc())
+
+    # Close the session
+    session.close()
+    
+    # Create a list of stations and their counts
+    station_list = [(item.station, item.count) for item in station_query]
+    
+    # Return a JSON list of stations from the dataset
+    return jsonify(station_list)    
 
 
 @app.route('/api/v1.0/tobs')
 def tobs():
-    print('This is the Temperature Observed Page')
-    return f'Temperature Observed Page'
+    
+    # Create a session link from Python to the DB
+    session = Session(engine)
+    
+    # Query all date and precipitation scores, starting from the most recent data point
+    most_recent_date = session.query(func.max(Measurement.date))
+    most_recent_date_pt = most_recent_date.scalar()
+    most_recent_date_object = datetime.strptime(most_recent_date_pt, "%Y-%m-%d")
+    
+    # Calculate the date one year from the last date in data set.
+    date_twelve_mos_ago = most_recent_date_object - timedelta(days=365)
+    
+    # Design a query to find the most active stations (i.e. which stations have the most rows?)
+    # List the stations and their counts in descending order.
+    station_query = session.query(Measurement.station,func.count(Measurement.station).label('count'))\
+                .group_by(Measurement.station).order_by(func.count(Measurement.station).desc())
 
+    # Close the session
+    session.close()
+    
+    
+    # Extract them most active station from station_query using the .first() method.
+    most_active_station = station_query.first()
+    most_active_station_id = most_active_station[0]    
+        
+    # Using the most active station id
+    # Query the last 12 months of temperature observation data (tobs) for station most_active_station_id
+    # tobs_query = session.query(Measurement.tobs).filter(Measurement.station == most_active_station_id, Measurement.date > date_twelve_mos_ago)
+    tobs_query = session.query(Measurement.tobs, Measurement.date).filter(Measurement.date > date_twelve_mos_ago)
+
+    # Create a list of stations and their counts
+    tobs_list = [(item.date, item.tobs) for item in tobs_query]
+    
+    # Return a JSON list of temperature observations for each date from the previous year
+    return jsonify(tobs_list)
+
+# Let's create a route to handle the form submission from the index page
+@app.route('/api/v1.0/temperature_stats')
+def temperature_stats():
+    start = request.args.get('start')
+    end = request.args.get('end')
+
+    if not start:
+        # Handle the case where start date is not provided
+        return "Start date is required.", 400
+
+    if end:
+        return redirect(url_for('temp_stats', start=start, end=end))
+    else:
+        return redirect(url_for('temp_stats', start=start))
+
+
+@app.route('/api/v1.0/<start>')        #makes the end date as optional on this line
+@app.route('/api/v1.0/<start>/<end>')
+def temp_stats(start, end=None):
+    # Create a session link from Python to the DB
+    session = Session(engine)
+    
+    # Convert start (and optional end date) strings to datetime object
+    start_date = datetime.strptime(start, "%Y-%m-%d")
+    end_date = datetime.strptime(end, "%Y-%m-%d") if end else None
+    
+    # Perform a query to retrieve the temperature statistics 
+    if end_date:
+        results = session.query(func.min(Measurement.tobs),
+                                func.avg(Measurement.tobs),
+                                func.max(Measurement.tobs))\
+                          .filter(Measurement.date >=start_date)\
+                          .filter(Measurement.date <= end_date).all() 
+    else:
+        results = session.query(func.min(Measurement.tobs),
+                                func.avg(Measurement.tobs),
+                                func.max(Measurement.tobs))\
+                          .filter(Measurement.date >=start_date).all()
+    session.close()
+    
+    
+    # Unpack the query result
+    tmin, tavg, tmax = results[0]
+    
+    # Format the data to be returned as JSON
+    temp_stats_data = {
+        'Start Date': start,
+        'End Date': end if end else 'Present',
+        'Min Temperature': tmin,
+        'Average Temperature': tavg,
+        'Max Temperature': tmax       
+    }
+    
+    return jsonify(temp_stats_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
+# END OF CODE
